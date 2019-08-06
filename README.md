77ed996483810d557308ac4b9d3d392b977bd463
Edward Jose <edward@me.com>
2019-08-06 18:58:56 +0800

    completed the tutorial

---
+++ README.md
@@ -0,0 +1,1 @@
+From this tutorial: https://testdriven.io/blog/developing-a-single-page-app-with-flask-and-vuejs/#post-route
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import BootstrapVue from 'bootstrap-vue';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
